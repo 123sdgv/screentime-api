256faaaf8705a1b6adfe82f28232e1725b63fe4c
Raven <llam1134@outlook.com>
2026-06-03 13:23:10 +0800

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <main style={{ fontFamily: "system-ui", padding: 24 }}>
+      <h1>ScreenTime API OK</h1>
+      <p>接口已运行。</p>
+    </main>
+  );
+}
